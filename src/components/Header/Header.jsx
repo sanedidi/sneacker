34d3f9ee674dd6f866f19c18d3c8de2fe9ba8e45
Shaxzod <sanedidi98@gmail.com>
2024-01-08 16:03:11 +0500

    project updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,103 +2,117 @@ import { Link } from "react-router-dom";
 import logo from "./../../assets/img/logo.webp";
 import logoMini from "./../../assets/img/logo-mini.webp";
 import "./Header.scss";
+import slider from "./../../assets/img/slider4Img.webp";
+import { useState } from "react";
 const Header = () => {
+  const [category, setCategory] = useState(0);
   return (
     <>
       <header className="header">
         <div className="header__bg"></div>
         <div className="container">
           <div className="header__wrapper">
-            <nav className="header__nav">
-              <Link className="header__link" to={"/"}>
-                LATEST
-              </Link>
-              <Link className="header__link" to={"/"}>
-                SNEACKERS
-              </Link>
-              <Link className="header__link" to={"/"}>
-                MEN
-              </Link>
-              <Link className="header__link" to={"/"}>
-                WOMEN
-              </Link>
-              <Link className="header__link" to={"/"}>
-                SALE
-              </Link>
-            </nav>
-            <img className="header__logo" src={logo} alt="" />
-            <div class="header__burger">
-              <span></span>
-            </div>
-            <img className="header__mini-logo" src={logoMini} alt="" />
-            <div className="header__tools">
-              <button className="header__search">
-                <span>SEARCH</span>
+            <div className="header__top">
+              <img className="header__logo" src={logo} alt="" />
+              <div class="header__burger">
+                <span></span>
+              </div>
+              <img className="header__mini-logo" src={logoMini} alt="" />
+              <div className="header__tools">
+                <input
+                  className="header__search"
+                  placeholder="What are you looking for?"
+                  type="text"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div className="header__btn">
                 <svg
-                  class="w-5 h-5"
+                  className="header__like"
+                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 22 20"
                 >
                   <path
-                    d="m13.758 12.916 5.167 5.166a.596.596 0 0 1 0 .843m-5.167-6.01 5.097 5.94m-5.097-5.94a7.269 7.269 0 0 0 1.736-4.718C15.494 4.173 12.221.9 8.197.9 4.173.9.9 4.173.9 8.197c0 4.024 3.273 7.297 7.297 7.297 1.734 0 3.4-.614 4.719-1.735l5.167 5.166m-4.325-6.01 4.396 5.94m.771.07-.07-.07m.07.07-.07-.07m.07.07a.592.592 0 0 1-.842 0m.772-.07a.492.492 0 0 1-.701 0m-.071.07.07-.07m-.07.07.07-.07m-9.956-4.553a6.112 6.112 0 0 1-6.105-6.105 6.112 6.112 0 0 1 6.105-6.105 6.113 6.113 0 0 1 6.105 6.105 6.113 6.113 0 0 1-6.105 6.105Z"
+                    d="M12.251 2.546c-.57.523-1 1.251-1.25 1.746-.25-.49-.682-1.224-1.253-1.746-1.032-.945-2.405-1.488-3.768-1.488-2.16 0-4.065 1.502-4.748 3.725-.594 1.936-.228 4.08.973 6.236 1.201 2.155 3.24 4.328 6.006 6.331a54.703 54.703 0 0 0 2.094 1.4c.197.125.444.192.695.192.252 0 .498-.067.697-.193l.025-.017c.222-.142 1.383-.886 2.067-1.382 2.766-2.003 4.805-4.176 6.006-6.331 1.201-2.156 1.567-4.3.973-6.236-.682-2.223-2.588-3.725-4.748-3.725-1.363 0-2.736.543-3.769 1.488ZM11.03 17.62a.14.14 0 0 1-.03.003.14.14 0 0 1-.03-.003 53.245 53.245 0 0 1-2.017-1.348l-.058.081.058-.08c-2.546-1.845-4.421-3.806-5.534-5.715C2.307 8.65 1.962 6.803 2.46 5.18c.516-1.681 1.934-2.802 3.52-2.802 1.049 0 2.11.42 2.912 1.153.317.29.592.7.797 1.057.2.351.329.646.358.715.124.354.295.568.473.693a.83.83 0 0 0 .48.154.845.845 0 0 0 .458-.14c.183-.118.363-.328.487-.688.016-.036.137-.325.337-.682.207-.369.493-.803.826-1.109.801-.733 1.863-1.153 2.912-1.153 1.586 0 3.004 1.12 3.52 2.802.498 1.623.153 3.47-.96 5.377-1.112 1.909-2.987 3.87-5.534 5.714a52.723 52.723 0 0 1-2.016 1.348Z"
                     fill="#000"
                     stroke="#000"
-                    stroke-width=".2"
+                    stroke-width=".4"
                   ></path>
                 </svg>
-              </button>
-              <Link className="header__acc" to={"/"}>
-                <span>ACCOUNT</span>
                 <svg
-                  class="w-5 h-5"
+                  className="header__shop"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
                   <path
-                    d="M10.278 10.1a4.48 4.48 0 0 0 4.475-4.475 4.48 4.48 0 0 0-4.475-4.475 4.48 4.48 0 0 0-4.475 4.475 4.48 4.48 0 0 0 4.475 4.475Zm0-7.732a3.26 3.26 0 0 1 3.257 3.257 3.26 3.26 0 0 1-3.257 3.257 3.26 3.26 0 0 1-3.257-3.257 3.26 3.26 0 0 1 3.257-3.257Z"
+                    d="M16.414 17.84v.076H3.585V7.089h2.42v2.098a.593.593 0 0 0 1.183 0V7.09h5.623v2.098a.593.593 0 0 0 1.184 0V7.09h2.42V17.84Zm.025-11.935h-2.444v-1.01c0-1.205-.39-2.206-1.09-2.905-.7-.7-1.7-1.09-2.905-1.09-1.206 0-2.206.39-2.906 1.09-.7.7-1.09 1.7-1.09 2.905v1.01H3.56c-.64 0-1.159.52-1.159 1.159v10.877c0 .64.52 1.16 1.16 1.16h12.878a1.16 1.16 0 0 0 1.159-1.16V7.064c0-.64-.52-1.16-1.159-1.16Zm-9.25-1.01c0-1.047.365-1.745.891-2.183.53-.441 1.232-.628 1.92-.628.687 0 1.39.187 1.919.628.526.438.892 1.136.892 2.183v1.01H7.188v-1.01Z"
                     fill="#000"
                     stroke="#000"
-                    stroke-width=".4"
-                  ></path>
-                  <path
-                    d="M10.24 11.602c-7.03 0-8.57 3.56-8.57 4.932 0 1.387 1.142 2.216 2.246 2.216h12.65c1.104 0 2.246-.829 2.246-2.216 0-1.372-1.54-4.931-8.571-4.931Zm6.326 6.046H3.916c-.555 0-1.144-.39-1.144-1.114 0-.993 1.372-3.83 7.47-3.83 6.095 0 7.468 2.837 7.468 3.83 0 .724-.59 1.114-1.144 1.114Z"
-                    fill="#000"
-                    stroke="#000"
-                    stroke-width=".4"
+                    stroke-width=".2"
                   ></path>
                 </svg>
-              </Link>
-              <svg
-                className="header__like"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 22 20"
-              >
-                <path
-                  d="M12.251 2.546c-.57.523-1 1.251-1.25 1.746-.25-.49-.682-1.224-1.253-1.746-1.032-.945-2.405-1.488-3.768-1.488-2.16 0-4.065 1.502-4.748 3.725-.594 1.936-.228 4.08.973 6.236 1.201 2.155 3.24 4.328 6.006 6.331a54.703 54.703 0 0 0 2.094 1.4c.197.125.444.192.695.192.252 0 .498-.067.697-.193l.025-.017c.222-.142 1.383-.886 2.067-1.382 2.766-2.003 4.805-4.176 6.006-6.331 1.201-2.156 1.567-4.3.973-6.236-.682-2.223-2.588-3.725-4.748-3.725-1.363 0-2.736.543-3.769 1.488ZM11.03 17.62a.14.14 0 0 1-.03.003.14.14 0 0 1-.03-.003 53.245 53.245 0 0 1-2.017-1.348l-.058.081.058-.08c-2.546-1.845-4.421-3.806-5.534-5.715C2.307 8.65 1.962 6.803 2.46 5.18c.516-1.681 1.934-2.802 3.52-2.802 1.049 0 2.11.42 2.912 1.153.317.29.592.7.797 1.057.2.351.329.646.358.715.124.354.295.568.473.693a.83.83 0 0 0 .48.154.845.845 0 0 0 .458-.14c.183-.118.363-.328.487-.688.016-.036.137-.325.337-.682.207-.369.493-.803.826-1.109.801-.733 1.863-1.153 2.912-1.153 1.586 0 3.004 1.12 3.52 2.802.498 1.623.153 3.47-.96 5.377-1.112 1.909-2.987 3.87-5.534 5.714a52.723 52.723 0 0 1-2.016 1.348Z"
-                  fill="#000"
-                  stroke="#000"
-                  stroke-width=".4"
-                ></path>
-              </svg>
-              <svg
-                className="header__shop"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M16.414 17.84v.076H3.585V7.089h2.42v2.098a.593.593 0 0 0 1.183 0V7.09h5.623v2.098a.593.593 0 0 0 1.184 0V7.09h2.42V17.84Zm.025-11.935h-2.444v-1.01c0-1.205-.39-2.206-1.09-2.905-.7-.7-1.7-1.09-2.905-1.09-1.206 0-2.206.39-2.906 1.09-.7.7-1.09 1.7-1.09 2.905v1.01H3.56c-.64 0-1.159.52-1.159 1.159v10.877c0 .64.52 1.16 1.16 1.16h12.878a1.16 1.16 0 0 0 1.159-1.16V7.064c0-.64-.52-1.16-1.159-1.16Zm-9.25-1.01c0-1.047.365-1.745.891-2.183.53-.441 1.232-.628 1.92-.628.687 0 1.39.187 1.919.628.526.438.892 1.136.892 2.183v1.01H7.188v-1.01Z"
-                  fill="#000"
-                  stroke="#000"
-                  stroke-width=".2"
-                ></path>
-              </svg>
+              </div>
             </div>
           </div>
         </div>
+        <nav className="header__nav">
+          <div
+            onMouseEnter={() => setCategory(1)}
+            onMouseLeave={() => setCategory(0)}
+            className="header__link"
+          >
+           <p>LATEST</p> 
+            <div className={`${category === 1 ? "active" : "test"}`}>
+        
+          <div className="header__categories-latest">
+            <div className="header__1">
+              <div className="header__top-1">
+                <Link to={"/"}>SHOP BY CATEGORY</Link>
+              </div>
+              <Link to={"/"}>SNEAKERS</Link>
+              <Link to={"/"}>CLOTHING</Link>
+              <Link to={"/"}>ACCESSORIES</Link>
+              <Link to={"/"}>SALE</Link>
+            </div>
+            <div className="header__1">
+              <div className="header__top-1">
+                <Link to={"/"}>MEN</Link>
+              </div>
+              <Link to={"/"}>SNEAKERS</Link>
+              <Link to={"/"}>CLOTHING</Link>
+            </div>
+            <div className="header__1">
+              <div className="header__top-1">
+                <Link to={"/"}>WOMEN</Link>
+              </div>
+              <Link to={"/"}>SNEAKERS</Link>
+              <Link to={"/"}>CLOTHING</Link>
+            </div>
+          </div>
+       
+
+      </div>
+          </div>
+          <Link className="header__link" to={"/"}>
+            SNEACKERS
+          </Link>
+          <Link className="header__link" to={"/"}>
+            MEN
+          </Link>
+          <Link className="header__link" to={"/"}>
+            WOMEN
+          </Link>
+          <Link className="header__link" to={"/"}>
+            SALE
+          </Link>
+        </nav>
       </header>
+
+   
+      <img src={slider} alt="" />
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import Header from '../components/Header/Header'
-import Hero from '../components/Hero/Hero'
-import Popular from '../components/Popular/Popular'
-import Brands from '../components/Brands/Brands'
-import Asics from '../components/Asics/Asics'
-import ProductCard from '../components/Product-Card/ProductCard'
-import Stepney from '../components/Stepney/Stepney'
 const Home = () => {
   return (
     <>
         <Header />
-        <Hero />
-        <Popular />
-        <Brands />
-        <Asics />
-        <ProductCard />
-        <Stepney />
+        {/* <Popular /> */}
+        {/* <Brands /> */}
     </>
   )
 }
